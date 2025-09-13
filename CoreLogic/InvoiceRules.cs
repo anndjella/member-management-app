bb3def397a96b3633c065b5f892c59a9d901a66a
anndjella <stankovicandjela53@gmail.com>
2025-09-13 22:17:40 +0200

    add first xUnit test and CI step

---
+++ CoreLogic/InvoiceRules.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogic
+{
+    public static class InvoiceRules
+    {
+        public static bool ExistsForCurrentMonth<T>(
+            IEnumerable<T> invoices,
+            DateTime now,
+            Func<T, int> getMonth,  
+            Func<T, int> getYear)   
+        {
+            int m = now.Month;
+            int y = now.Year;
+            return invoices.Any(i => getMonth(i) == m && getYear(i) == y);
+        }
+    }
+}
